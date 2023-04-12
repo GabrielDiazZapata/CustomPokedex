cc2eb81b5687d7a878d687188abc89715d1af6d3
GabrielDiazZapata <126862544+GabrielDiazZapata@users.noreply.github.com>
2023-04-12 17:45:47 +0100

    Aqui pusimos los paquetes ordenados. Tambien creamos dos clases, Pokemon y CustomPokedex, En Pokemon añadimos los atributos y añadimos los constructores.

---
+++ src/net/salesianos/pokedex/CustomPokedex.java
@@ -0,0 +1,6 @@
+package net.salesianos.pokedex;
+public class CustomPokedex {
+    public static void main(String[] args) throws Exception {
+        System.out.println("¡Bienvenido a CustomPokedex!");
+    }
+}
---
+++ src/net/salesianos/pokemones/Pokemon.java
@@ -0,0 +1,18 @@
+package net.salesianos.pokemones;
+public class Pokemon {
+    private String Nombre;
+    private String Tipo;
+    private double Altura;
+    private double Peso;
+    private String Descripcion;
+
+
+    public Pokemon(String Nombre, String Tipo, double Altura, double Peso, String Descripcion){
+        this.Nombre = Nombre;
+        this.Tipo = Tipo;
+        this.Altura = Altura;
+        this.Peso = Peso;
+        this.Descripcion = Descripcion;
+    }
+
+}
