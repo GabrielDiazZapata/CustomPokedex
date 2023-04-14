09037dc11f4652e29736cd47d893e9956bd09844
GabrielDiazZapata <126862544+GabrielDiazZapata@users.noreply.github.com>
2023-04-14 09:44:40 +0100

    Despues de haber creado el paquete de la lista y haber creado la clase, lo que hice fue crear una lista inteligente, a la que le cree varios metodos necesarios, y que para el usuario le sea facil la interfaz.

--- src/net/salesianos/lista/listaInteligente.java
+++ src/net/salesianos/lista/listaInteligente.java
@@ -1,5 +1,53 @@
 package net.salesianos.lista;
+import java.util.Arrays;
 
+import net.salesianos.pokemones.*;
 public class listaInteligente {
-    
+private pokemon[] lista;
+private int cantidad;
+
+   public listaInteligente() {
+    lista = new pokemon[1];
+    cantidad = 0;
+   }
+
+   public void agregarPokemon(pokemon p) {
+    if(cantidad == lista.length) {
+        lista = Arrays.copyOf(lista, lista.length * 2);
+    }
+    lista[cantidad] = p;
+    cantidad++;
+    System.out.println("Pokemon agregado correctamente gracias por nada bobo.");
+   }
+
+   public void eliminarPokemon(String Nombre) {
+    for (int i = 0; i < cantidad; i++) {
+        if(lista[i].getNombre().equals(Nombre)) {
+            for(int j = i; j < cantidad - 1; j++) {
+                lista[j] = lista[j + 1];
+            }
+            cantidad--;
+            lista[cantidad] = null;
+            System.out.println("Pokemon eliminado correctamente ahora creame otro soplagaitas, y mas te vale que sea bueno.");
+            return;
+        }
+    }
+    System.out.println("No se encontró el Pokemon en la lista, aprende a escribir.");
+
+   }
+
+   public void mostrarPokemon() {
+        System.out.println("Lista de Pokemons:");
+        for (int i = 0; i < cantidad; i++) {
+            System.out.println((i+1) + ". " + lista[i].toString());
+        }
+   }
+
+   public pokemon getPokemon(int posicion) {
+    if (posicion < 0 || posicion >= cantidad) {
+        return null;
+    }
+    return lista[posicion];
+   }
+
 }
