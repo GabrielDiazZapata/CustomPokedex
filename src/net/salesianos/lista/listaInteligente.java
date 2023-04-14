9e966d8d0ece0d7720d18659abdf570f7fbddb2d
GabrielDiazZapata <126862544+GabrielDiazZapata@users.noreply.github.com>
2023-04-14 09:50:37 +0100

    Acomodando la identacion del codigo

--- src/net/salesianos/lista/listaInteligente.java
+++ src/net/salesianos/lista/listaInteligente.java
@@ -6,21 +6,21 @@ public class listaInteligente {
 private pokemon[] lista;
 private int cantidad;
 
-   public listaInteligente() {
+public listaInteligente() {
     lista = new pokemon[1];
     cantidad = 0;
-   }
+}
 
-   public void agregarPokemon(pokemon p) {
+public void agregarPokemon(pokemon p) {
     if(cantidad == lista.length) {
         lista = Arrays.copyOf(lista, lista.length * 2);
     }
     lista[cantidad] = p;
     cantidad++;
     System.out.println("Pokemon agregado correctamente gracias por nada bobo.");
-   }
+}
 
-   public void eliminarPokemon(String Nombre) {
+public void eliminarPokemon(String Nombre) {
     for (int i = 0; i < cantidad; i++) {
         if(lista[i].getNombre().equals(Nombre)) {
             for(int j = i; j < cantidad - 1; j++) {
@@ -34,20 +34,20 @@ private int cantidad;
     }
     System.out.println("No se encontró el Pokemon en la lista, aprende a escribir.");
 
-   }
+}
 
-   public void mostrarPokemon() {
+public void mostrarPokemon() {
         System.out.println("Lista de Pokemons:");
         for (int i = 0; i < cantidad; i++) {
             System.out.println((i+1) + ". " + lista[i].toString());
         }
-   }
+}
 
-   public pokemon getPokemon(int posicion) {
+public pokemon getPokemon(int posicion) {
     if (posicion < 0 || posicion >= cantidad) {
         return null;
     }
     return lista[posicion];
-   }
+}
 
 }
