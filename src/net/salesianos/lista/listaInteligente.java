ffe4e4d52f0a6874fdc1c73c524659d692264c18
GabrielDiazZapata <126862544+GabrielDiazZapata@users.noreply.github.com>
2023-04-18 08:38:53 +0100

    Este es el segundo metodo de la clase lista inteligente, que es eliminar a un pokemon de la lista

--- src/net/salesianos/lista/listaInteligente.java
+++ src/net/salesianos/lista/listaInteligente.java
@@ -20,6 +20,22 @@ public void agregarPokemon(pokemon p) {
     System.out.println("Pokemon agregado correctamente gracias por nada bobo.");
 }
 
+public void eliminarPokemon(String Nombre) {
+    for (int i = 0; i < cantidad; i++) {
+        if(lista[i].getNombre().equals(Nombre)) {
+            for(int j = i; j < cantidad - 1; j++) {
+                lista[j] = lista[j + 1];
+            }
+            cantidad--;
+            lista[cantidad] = null;
+            System.out.println("Pokemon eliminado correctamente ahora creame otro soplagaitas, y mas te vale que sea bueno.");
+            return;
+        }
+    }
+    System.out.println("No se encontró el Pokemon en la lista, aprende a escribir.");
+
+}
+
 
 
 }
