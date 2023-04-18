450f1df56a877749f49770fcc4589eb23fd941c8
GabrielDiazZapata <126862544+GabrielDiazZapata@users.noreply.github.com>
2023-04-18 09:44:00 +0100

    Hemos creado dos listas, una de capturados y otra de avistados y hemos creado un menu de interaccion con el usuario.

--- src/net/salesianos/pokedex/CustomPokedex.java
+++ src/net/salesianos/pokedex/CustomPokedex.java
@@ -1,6 +1,30 @@
 package net.salesianos.pokedex;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import net.salesianos.pokemones.Pokemon;
+
 public class CustomPokedex {
     public static void main(String[] args) throws Exception {
         System.out.println("¡Bienvenido a CustomPokedex!");
+
+        ArrayList<Pokemon> avistados = new ArrayList<Pokemon>();
+        ArrayList<Pokemon> capturados = new ArrayList<Pokemon>();
+
+        Scanner teclado = new Scanner(System.in);
+        String opcion = "";
+
+        while (!opcion.equals("f")) {
+            System.out.println("\nMenu de interaccion:");
+            System.out.println("a) Añadir un pokemon avistado");
+            System.out.println("b) Añadir avistamiento legendario");
+            System.out.println("c) Pasar a capturados");
+            System.out.println("d) Mostrar lista de avistados");
+            System.out.println("e) Mostrar lista de caputrados");
+            System.out.println("f) Cerrar la Pokédex");
+            System.out.println("Selecciona una opción");
+            opcion = teclado.nextLine();
+        }
     }
 }
